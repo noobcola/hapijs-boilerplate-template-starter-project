f416d764003ebec1b9be90e48c7e0c0be3875464
Thomas Le <noobcola@gmail.com>
2016-07-19 10:53:49 -0700

    Added AJAX calls to loan submission form

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -4,3 +4,22 @@ function showAlert(type, message){
     alert.find('.message').html(message);
     $('.alert-container').html(alert);
 }
+
+function callApi(params, callback){
+    var opts = {
+        method: "GET",
+        url: "http://localhost:8000" + params.endpoint,
+        dataType: 'json',
+        async: true,
+        success: function(data){
+            callback(null, data);
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            callback(errorThrown);
+        }
+    }
+
+    Object.assign(opts, params);
+
+    $.ajax(opts);
+}
